8d423ec60ca60ba003d480f6bb9f0cb25c8b5962
serhiichaiuk <chayck7@gmail.com>
2022-07-12 18:19:08 +0300

    modified the vue template

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     quotes: ['error', 'single'],
     'no-multi-spaces': ['error'],
     'no-unused-vars': 'off',
+    'vue/multi-word-component-names': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
