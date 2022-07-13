2e0136cd04fba653951e4285f3db7252ef17b7c0
serhiichaiuk <chayck7@gmail.com>
2022-07-13 14:46:08 +0300

    made data available everywhere

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    <Navigation />
-    <router-view />
+    <Navigation :user="user" />
+    <router-view :user="user" />
   </div>
 </template>
 
 <script>
+import db from './db.js'
 import Navigation from '@/components/Navigation'
 export default {
+  name: 'App',
+  data: function () {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('L4tZgxmlZMWnLEusWtRV')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  },
   components: {
     Navigation
   }
